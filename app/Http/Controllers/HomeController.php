7339d2a7eae2a4aebdaffae6a76991923f5979fe
Muhammad Khairudin <om.joko@icloud.com>
2022-07-20 12:44:56 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return redirect()->to("/");
+        return redirect()->to("/admin");
         // return view('home');
     }
 }
